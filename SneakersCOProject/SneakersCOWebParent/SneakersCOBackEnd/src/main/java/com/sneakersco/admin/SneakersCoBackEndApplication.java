b9b02b9a700574501be8e348a326a17144f6c079
Nastya Gordeeva <nastyagordeeva72@gmail.com>
2024-03-04 19:48:32 +0200

    M-Initial set up

---
+++ SneakersCOProject/SneakersCOWebParent/SneakersCOBackEnd/src/main/java/com/sneakersco/admin/SneakersCoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.sneakersco.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakersCoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakersCoBackEndApplication.class, args);
+	}
+
+}
---
+++ SneakersCOProject/SneakersCOWebParent/SneakersCOFrontEnd/src/main/java/com/sneakersco/site/SneakersCoFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.sneakersco.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakersCoFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakersCoFrontEndApplication.class, args);
+	}
+
+}
