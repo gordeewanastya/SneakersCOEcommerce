cbe8ab93f863ccb955acab70555fbf1dc6a4b997
Nastya Gordeeva <nastyagordeeva72@gmail.com>
2024-03-06 22:16:50 +0200

    M-First steps on user managing table

--- SneakersCOProject/SneakersCOWebParent/SneakersCOBackEnd/src/main/java/com/sneakersco/admin/user/controller/UserController.java
+++ SneakersCOProject/SneakersCOWebParent/SneakersCOBackEnd/src/main/java/com/sneakersco/admin/user/controller/UserController.java
@@ -2,9 +2,13 @@ package com.sneakersco.admin.user.controller;
 
 import com.sneakersco.admin.user.service.UserService;
 import com.sneakersco.admin.user.service.implementation.UserServiceImpl;
+import com.sneakersco.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -13,7 +17,9 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/users")
-    public String listAll(){
+    public String listAll(Model model){
+        List<User> listUsers = userService.listAll();
+        model.addAttribute("listUsers", listUsers);
         return "users";
     }
 }
